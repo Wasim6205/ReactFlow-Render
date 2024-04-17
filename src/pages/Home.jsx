223be3347304659d2ab0879932918af3180101b7
Wasim <wasimbhai6205@gmail.com>
2024-04-17 18:02:40 +0530

    ReactFlow render app created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactFlowRenderer from '../ReactFlowRenderer'
+
+const Home = () => {
+  return (
+    <div>
+        <ReactFlowRenderer />
+    </div>
+  )
+}
+
+export default Home
